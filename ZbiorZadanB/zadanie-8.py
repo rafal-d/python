d260470befc7b9543c4711d6865548bf1fd66946
rafal-d <rafal-d@users.noreply.github.com>
2020-05-08 13:03:01 +0100

    rownanie

---
+++ ZbiorZadanB/zadanie-8.py
@@ -0,0 +1,13 @@
+# Zbór zadań B - zadanie 8
+# Napisz program rozwiązujący równanie (x+y) * (x+y)
+# autor :  Rafał D.
+
+print('Program rozwiązujący równanie ( x + y ) * ( x + y ) \n')
+print('Podaj liczbe x:')
+x = int(input())
+print('Podaj liczbe y:')
+y = int(input())
+
+rownanie = ( x + y ) * ( x + y )
+
+print(rownanie)
