26d76eec7fa784b015e5593183fa5707e63458d5
rafal-d <rafal-d@users.noreply.github.com>
2020-05-08 13:04:08 +0100

    lista kolory

---
+++ ZbiorZadanB/zadanie-5.py
@@ -0,0 +1,13 @@
+# Zbór zadań B - zadanie 5
+# Napisz program wyświetlający pierwszy i ostatni kolo z podanej listy
+# Kolory = ['czerwony','zielony','niebieski','czarny']
+# autor :  Rafał D.
+
+Kolory = ['czerwony','zielony','niebieski','czarny']
+
+print('Napisz program wyświetlający pierwszy i ostatni kolor z podanej listy \n')
+print(Kolory,'\n')
+print('Pierwszy kolo na liście to: ',Kolory[0])
+print('Czwarty kolor na liście to: ',Kolory[3])
+
+
